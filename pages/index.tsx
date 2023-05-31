07a81085a7f0a0bfd6e6c734c699c81d652a097b
Sanskar <sanskaaragrawal2001@gmail.com>
2023-06-01 00:53:21 +0530

    Adding responsive header

--- pages/index.tsx
+++ pages/index.tsx
@@ -7,9 +7,7 @@ export default function Home () {
     <main className='w-full flex flex-col items-center'>
       {/* Header */}
       <section className='w-full bg-island-background h-[30rem] bg-fixed sm:h-[35rem] bg-cover bg-center bg-no-repeat flex items-center flex-col gap-y-[4.5rem] justify-center sm:justify-start'>
-        <header className='relative w-full hidden justify-center items-center mt-6 sm:flex'>
-          <Header />
-        </header>
+        <Header />
         <h1 className='font-bold text-[4rem] leading-[3.75rem] sm:text-[8.75rem] text-center hero-gradient-text sm:leading-[8.25rem] tracking-[-23%]'>
           Welcome
           <br /> to Hawaii
